9e9abf58f7f1fdb88eb01ecd43846f726d5b2bdb
sdey6 <74349228+sdey6@users.noreply.github.com>
2024-01-18 00:21:17 +0100

    Update case_study.py
    modifying comments

--- case_study.py
+++ case_study.py
@@ -6,10 +6,6 @@ import requests
 import pandas as pd
 import isodate
 
-pd.options.display.width = None
-pd.options.display.max_columns = None
-pd.set_option('display.max_rows', 3000)
-pd.set_option('display.max_columns', 3000)
 
 url = "https://bnlf-tests.s3.eu-central-1.amazonaws.com/recipes.json"
 
